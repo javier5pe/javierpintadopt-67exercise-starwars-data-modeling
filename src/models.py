8bf37d9eb0ca106aeab75b8bd5bebf6061b5f725
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-07-01 18:12:03 +0000

    a

--- src/models.py
+++ src/models.py
@@ -12,7 +12,12 @@ class Person(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -24,9 +29,44 @@ class Address(Base):
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+
+class Usuarios(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Favoritos_personajes(Base):
+    __tablename__ = 'favoritos_personajes'
+    id = Column(Integer, primary_key=True, nullable=False)
+    personajes_relacion = Column(Integer, ForeignKey(Personajes.id), nullable=False)
+    usuarios_relacion = Column(Integer, ForeignKey(Usuarios.id), nullable=False)
+
+class Favoritos_vehiculos(Base):
+    __tablename__ = 'favoritos_vehiculos'
+    id = Column(Integer, primary_key=True, nullable=False)
+    vehiculos_relacion = Column(Integer, ForeignKey(Vehiculos.id), nullable=False)
+    usuarios_relacion = Column(Integer, ForeignKey(Usuarios.id), nullable=False)
+
+class Favoritos_planetas(Base):
+    __tablename__ = 'favoritos_planetas'
+    id = Column(Integer, primary_key=True, nullable=False)
+    planetas_relacion = Column(Integer, ForeignKey(Planetas.id), nullable=False)
+    usuarios_relacion = Column(Integer, ForeignKey(Usuarios.id), nullable=False)
 
     def to_dict(self):
         return {}
-
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+    
+    render_er(Base, 'diagram.png')
